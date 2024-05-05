86197c46af6d8a5d1e44746611df5e02f15e2fba
Bogusz Kaszowski <bogusz.kaszowski@callstack.com>
2024-05-05 20:02:31 +0200

    navigation & search

--- JPApexPredators/ContentView.swift
+++ JPApexPredators/ContentView.swift
@@ -9,31 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     let predators = Predators()
+    @State var searchText = ""
+    
+    var filteredDinos: [ApexPredator] {
+        if (searchText.isEmpty) {
+            return predators.apexPredators
+        }
+        
+        return predators.apexPredators.filter {
+            predator in
+            predator.name.localizedCaseInsensitiveContains(searchText)
+        }
+        
+    }
 
     var body: some View {
-        List(predators.apexPredators) { predator in
-            HStack {
-                Image(predator.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
-                    .shadow(color: .white, radius: 1)
-                
-                VStack(alignment: .leading) {
-                    Text(predator.name)
-                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                    
-                    Text(predator.type.rawValue.capitalized)
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .padding(.horizontal, 13)
-                        .padding(.vertical, 5)
-                        .background(predator.type.background)
-                        .clipShape(.capsule)
-                    
+        NavigationStack {
+            List(filteredDinos) { predator in
+                NavigationLink{
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                } label: {
+                    HStack {
+                        Image(predator.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
+                            .shadow(color: .white, radius: 1)
+                        
+                        VStack(alignment: .leading) {
+                            Text(predator.name)
+                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            
+                            Text(predator.type.rawValue.capitalized)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.horizontal, 13)
+                                .padding(.vertical, 5)
+                                .background(predator.type.background)
+                                .clipShape(.capsule)
+                            
+                        }
+                        
+                    }
                 }
-                
             }
+            .navigationTitle("Apex Predators")
+            .searchable(text: $searchText)
+            .autocorrectionDisabled()
+            .animation(.default, value: searchText)
+            
         }
         .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
     }
