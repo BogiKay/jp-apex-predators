9f8f7208b223d2fbb498f5fa15c9ff7727dbee19
Bogusz Kaszowski <bogusz.kaszowski@callstack.com>
2024-05-05 18:38:06 +0200

    Initial Commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Bogusz Kaszowski on 05/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
